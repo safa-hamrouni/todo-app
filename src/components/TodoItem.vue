2e2bfefc45db17093e3d2f04cb4758bbf5a103d5
S.H <63782417+safa-hamrouni@users.noreply.github.com>
2023-10-08 14:47:34 +0100

    2.1: done, 2.2: done(visually distinguish checked task todo) 2.3: done

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+
+const props = defineProps({
+  todo: Object
+})
+</script>
+
+<template>
+  <div class="todo-item">
+    <input type="checkbox" :checked="todo.done"/>
+    <span class="todo-task">{{ todo.text }}</span>
+    <button>x</button>   
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.todo {
+  &-item {
+  // display: flex;
+  list-style-type: none;
+  }
+  &-task {
+    min-width: 100px;
+    padding-left: 5px;
+    padding-right: 5px;
+  }
+}
+</style>
---
+++ src/components/TodoView.vue
@@ -0,0 +1,40 @@
+<script setup>
+import TodoItem from './TodoItem.vue'
+import { ref } from 'vue'
+
+let id = 0
+const todos = ref([])
+const newTodo = ref('')
+
+function addTodo () {
+  if (newTodo.value.length === 0) {
+    alert("please fill in a task!");
+  } else {
+    todos.value.push({ id: id++, text: newTodo.value, done: false})
+    newTodo.value = ''
+  }
+}
+
+</script>
+
+<template>
+  <div class="greetings">
+    <h3>let's keep track of our Todos</h3>
+    <div>
+      <form @submit.prevent="addTodo">
+        <input v-model="newTodo" />
+        <button>submit</button>
+      </form>
+      <TodoItem
+        v-for="(todo, index) in todos"
+        :key="index"
+        :todo="todo"  
+      />
+    </div>
+    
+  </div>
+</template>
+
+<style scoped>
+
+</style>
